ba5c30806cd7d50c8b2c97bf2f82db4044330992
mioe <mioe.boss@gmail.com>
2022-04-30 16:52:03 +0300

    feat: add sass & windicss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+/**
+ * Styles
+ */
+import 'virtual:windi.css'
+import './assets/sass/main.sass'
+
+
+/**
+ * init app
+ */
 createApp(App).mount('#app')
